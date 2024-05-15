d5726ba3b706f4a7d81b6c7b9ffda181d089e547
Budi Gunawan <budiindra.g@gmail.com>
2024-05-15 21:45:55 +0700

    feat: install daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <h1 className='text-3xl font-bold underline'>Sahabit</h1>;
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Sahabit</h1>
+      <button className='btn'>Hello daisyUI</button>
+    </>
+  );
 }
 
 export default App;
