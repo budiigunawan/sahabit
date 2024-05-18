598f75ff1eeb3afd3f9793be0726ac04db059e4c
Budi Gunawan <budiindra.g@gmail.com>
2024-05-19 06:52:01 +0700

    feat: add habit seed file

--- src/App.tsx
+++ src/App.tsx
@@ -3,119 +3,13 @@ import { Header } from './components/header';
 import { Navbar } from './components/navbar';
 import { Habits } from './components/habits';
 import { Habit } from './types/habit-type';
+import { habitSeed } from './data';
 
 function App() {
   // const [point, setPoint] = useState<number>(3);
   // const [habits, setHabits] = useState<Habit[]>([]);
   const point = 3;
-  const habits: Habit[] = [
-    {
-      name: 'Exercise',
-      category: 'body',
-      variants: [
-        {
-          name: 'Push up',
-          levels: [
-            { level: 'gold', name: '15 times' },
-            { level: 'silver', name: '10 times' },
-            { level: 'bronze', name: '5 times' },
-          ],
-        },
-        {
-          name: 'Jogging',
-          levels: [
-            { level: 'gold', name: '5 km' },
-            { level: 'silver', name: '2.5 km' },
-            { level: 'bronze', name: '1 km' },
-          ],
-        },
-        {
-          name: 'Cycling',
-          levels: [
-            { level: 'gold', name: '60 minutes' },
-            { level: 'silver', name: '30 minutes' },
-            { level: 'bronze', name: '15 minutes' },
-          ],
-        },
-      ],
-      status: undefined,
-      habitDone: {
-        variantName: 'Jogging',
-        selectedLevel: {
-          level: 'bronze',
-          name: '1 km',
-        },
-      },
-    },
-    {
-      name: 'Study',
-      category: 'mind',
-      variants: [
-        {
-          name: 'Read book',
-          levels: [
-            { level: 'gold', name: '60 minutes' },
-            { level: 'silver', name: '30 minutes' },
-            { level: 'bronze', name: '15 minutes' },
-          ],
-        },
-        {
-          name: 'Leetcode',
-          levels: [
-            { level: 'gold', name: '3 questions' },
-            { level: 'silver', name: '2 questions' },
-            { level: 'bronze', name: '1 questions' },
-          ],
-        },
-        {
-          name: 'English',
-          levels: [
-            { level: 'gold', name: '60 minutes' },
-            { level: 'silver', name: '30 minutes' },
-            { level: 'bronze', name: '15 minutes' },
-          ],
-        },
-      ],
-      status: undefined,
-      habitDone: {
-        variantName: 'Leetcode',
-        selectedLevel: {
-          level: 'silver',
-          name: '2 questions',
-        },
-      },
-    },
-    {
-      name: 'Ritual',
-      category: 'soul',
-      variants: [
-        {
-          name: 'Meditation',
-          levels: [
-            { level: 'gold', name: '30 minutes' },
-            { level: 'silver', name: '15 minutes' },
-            { level: 'bronze', name: '5 minutes' },
-          ],
-        },
-        {
-          name: 'Yoga',
-          levels: [
-            { level: 'gold', name: '60 minutes' },
-            { level: 'silver', name: '30 minutes' },
-            { level: 'bronze', name: '15 minutes' },
-          ],
-        },
-      ],
-      status: undefined,
-      habitDone: {
-        variantName: 'Meditation',
-        selectedLevel: {
-          level: 'gold',
-          name: '30 minutes',
-        },
-      },
-    },
-  ];
+  const habits: Habit[] = habitSeed;
 
   return (
     <>
---
+++ src/data.ts
@@ -0,0 +1,108 @@
+export const habitSeed = [
+  {
+    name: 'Exercise',
+    category: 'body',
+    variants: [
+      {
+        name: 'Push up',
+        levels: [
+          { level: 'gold', name: '15 times' },
+          { level: 'silver', name: '10 times' },
+          { level: 'bronze', name: '5 times' },
+        ],
+      },
+      {
+        name: 'Jogging',
+        levels: [
+          { level: 'gold', name: '5 km' },
+          { level: 'silver', name: '2.5 km' },
+          { level: 'bronze', name: '1 km' },
+        ],
+      },
+      {
+        name: 'Cycling',
+        levels: [
+          { level: 'gold', name: '60 minutes' },
+          { level: 'silver', name: '30 minutes' },
+          { level: 'bronze', name: '15 minutes' },
+        ],
+      },
+    ],
+    status: undefined,
+    habitDone: {
+      variantName: 'Jogging',
+      selectedLevel: {
+        level: 'bronze',
+        name: '1 km',
+      },
+    },
+  },
+  {
+    name: 'Study',
+    category: 'mind',
+    variants: [
+      {
+        name: 'Read book',
+        levels: [
+          { level: 'gold', name: '60 minutes' },
+          { level: 'silver', name: '30 minutes' },
+          { level: 'bronze', name: '15 minutes' },
+        ],
+      },
+      {
+        name: 'Leetcode',
+        levels: [
+          { level: 'gold', name: '3 questions' },
+          { level: 'silver', name: '2 questions' },
+          { level: 'bronze', name: '1 questions' },
+        ],
+      },
+      {
+        name: 'English',
+        levels: [
+          { level: 'gold', name: '60 minutes' },
+          { level: 'silver', name: '30 minutes' },
+          { level: 'bronze', name: '15 minutes' },
+        ],
+      },
+    ],
+    status: undefined,
+    habitDone: {
+      variantName: 'Leetcode',
+      selectedLevel: {
+        level: 'silver',
+        name: '2 questions',
+      },
+    },
+  },
+  {
+    name: 'Ritual',
+    category: 'soul',
+    variants: [
+      {
+        name: 'Meditation',
+        levels: [
+          { level: 'gold', name: '30 minutes' },
+          { level: 'silver', name: '15 minutes' },
+          { level: 'bronze', name: '5 minutes' },
+        ],
+      },
+      {
+        name: 'Yoga',
+        levels: [
+          { level: 'gold', name: '60 minutes' },
+          { level: 'silver', name: '30 minutes' },
+          { level: 'bronze', name: '15 minutes' },
+        ],
+      },
+    ],
+    status: undefined,
+    habitDone: {
+      variantName: 'Meditation',
+      selectedLevel: {
+        level: 'gold',
+        name: '30 minutes',
+      },
+    },
+  },
+];
