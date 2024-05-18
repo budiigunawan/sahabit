75411961346d7dd321831c72bc5b633a3d7e4ec9
Budi Gunawan <budiindra.g@gmail.com>
2024-05-18 18:52:39 +0700

    feat: add props for header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,31 @@
+import { useState } from 'react';
 import { Header } from './components/header';
 import { Navbar } from './components/navbar';
 
+type Task = {
+  name: string;
+  category: string;
+  variants: TaskVariant[];
+};
+
+type TaskVariant = {
+  name: string;
+  levels: VariantLevel[];
+};
+
+type VariantLevel = {
+  level: string;
+  name: string;
+};
+
 function App() {
+  const [point, setPoint] = useState<number>(3);
+  const [tasks, setTasks] = useState<Task[]>([]);
+
   return (
     <>
       <Navbar />
-      <Header />
+      <Header point={point} tasksCounter={tasks.length} />
     </>
   );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,20 @@
 import goldMedal from '../assets/gold.png';
 
-export const Header = () => {
+type HeaderProps = {
+  point: number;
+  tasksCounter: number;
+};
+
+export const Header = ({ point, tasksCounter }: HeaderProps) => {
   return (
     <div className='max-w-2xl mx-auto py-3 px-5 '>
       <div className='flex justify-between'>
         <div className='flex flex-col justify-center'>
           <h1 className='text-xl font-semibold'>Hello Champ!</h1>
-          <p className='mt-1'>
-            I am here to cheer you on every step of the way.
+          <p className='mt-1 text-lg'>
+            {tasksCounter
+              ? 'Complete the task and earn a shiny medal.'
+              : 'Sahabit is here to cheer you on every step of the way.'}
           </p>
         </div>
         <div>
@@ -15,9 +22,9 @@ export const Header = () => {
         </div>
       </div>
       <progress
-        className='mt-8 progress progress-success w-full'
-        value='80'
-        max='100'
+        className='mt-8 progress progress-success w-full h-7'
+        value={point}
+        max='12'
       ></progress>
     </div>
   );
