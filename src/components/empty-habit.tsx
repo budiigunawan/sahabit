c0cb79bab40473b7d9b8042b58fcdba679d7211f
Budi Gunawan <budiindra.g@gmail.com>
2024-05-19 06:28:56 +0700

    feat: add empty habit

---
+++ src/components/empty-habit.tsx
@@ -0,0 +1,11 @@
+import emptyBox from '../assets/empty.svg';
+
+export const EmptyHabit = () => {
+  return (
+    <div className='max-w-2xl mx-auto text-center'>
+      <img src={emptyBox} alt='an empty box' className='mx-auto' />
+      <h2 className='text-lg font-semibold mt-8'>No habits found</h2>
+      <p className='mt-2'>Lets add a new habit by click button below!</p>
+    </div>
+  );
+};
--- src/components/habits.tsx
+++ src/components/habits.tsx
@@ -1,3 +1,4 @@
+import { EmptyHabit } from './empty-habit';
 import { HabitCard } from './habit-card';
 
 type Habit = {
@@ -30,9 +31,12 @@ type HabitsProps = {
 export const Habits = ({ data }: HabitsProps) => {
   return (
     <div className='max-w-2xl mx-auto py-3 px-5'>
-      {data.length &&
-        data.map((habit, idx) => <HabitCard key={idx} habit={habit} />)}
-      <button className='btn btn-success w-full mt-4'>Add habit</button>
+      {data?.length ? (
+        data?.map((habit, idx) => <HabitCard key={idx} habit={habit} />)
+      ) : (
+        <EmptyHabit />
+      )}
+      <button className='btn btn-success w-full mt-8'>Add habit</button>
     </div>
   );
 };
