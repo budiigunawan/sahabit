36bb01abb77740c0497a20ac809dfe7036fcff82
Budi Gunawan <budiindra.g@gmail.com>
2024-05-22 20:04:58 +0700

    feat: init add habit modal

--- src/components/habits.tsx
+++ src/components/habits.tsx
@@ -1,12 +1,19 @@
 import { EmptyHabit } from './empty-habit';
 import { HabitCard } from './habit-card';
 import { Habit } from '../types/habit-type';
+import { AddHabitModal } from './add-habit-modal';
 
 type HabitsProps = {
   habits: Habit[];
 };
 
 export const Habits = ({ habits }: HabitsProps) => {
+  const handleOpenAddHabitModal = () => {
+    (
+      document?.getElementById('add-habit-modal') as HTMLDialogElement
+    ).showModal();
+  };
+
   return (
     <div className='max-w-2xl mx-auto py-3 px-5'>
       {habits?.length ? (
@@ -14,7 +21,11 @@ export const Habits = ({ habits }: HabitsProps) => {
       ) : (
         <EmptyHabit />
       )}
-      <button className='btn btn-success w-full mt-8 text-white'>
+      <AddHabitModal />
+      <button
+        onClick={handleOpenAddHabitModal}
+        className='btn btn-success w-full mt-8 text-white'
+      >
         Add habit
       </button>
     </div>
