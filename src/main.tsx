e9e7f001d2a1cdf420e2eeda989b6ff939de2a0d
Budi Gunawan <budiindra.g@gmail.com>
2024-06-03 11:47:04 +0700

    init error page element

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import { Root } from './routes/root.tsx';
+import { ErrorPage } from './components/error-page.tsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
